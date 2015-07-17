e5d79c3094994381275cb5170dc384d8d53ef8b8
emelyanovtv <timofey@MacBook-Air.local>
2015-07-17 11:58:35 +0300

    final

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 (function(window){
 
     var _t = null;
+    window.requestAnimFrame = (function(){
+        return  window.requestAnimationFrame       ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame    ||
+            function(callback, element){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
     var palyerSHRI = function(obj, params)
     {
         return new playerSHRI.lib.init(obj,params);
